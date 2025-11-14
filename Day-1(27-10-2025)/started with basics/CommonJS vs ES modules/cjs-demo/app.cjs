9a712a7451f3762449130de2c29c1dc2950921cb
RITHIHA ELANGOVAN <166149801+rithuelan@users.noreply.github.com>
2025-11-14 20:39:35 +0530

    Delete Day-1(27-10-2025) directory

--- Day-1(27-10-2025)/started with basics/CommonJS vs ES modules/cjs-demo/app.cjs
+++
@@ -1,5 +0,0 @@
-// app.cjs — Importing CommonJS module
-const math = require('./math.cjs');
-
-console.log("Addition:", math.add(5, 10));
-console.log("Multiplication:", math.multiply(4, 3));
--- Day-1(27-10-2025)/started with basics/Understanding the event loop/exampleB.js
+++
@@ -1,9 +0,0 @@
-// Example B — show that nextTick runs before Promises
-
-console.log('start');
-
-Promise.resolve().then(() => console.log('promise'));
-process.nextTick(() => console.log('nextTick'));
-
-console.log('end');
-
